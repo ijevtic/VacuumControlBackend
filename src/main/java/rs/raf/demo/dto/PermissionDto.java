611b5bbc7db973501d78e7e555a46a51cbe949c3
Ivan Jevtic <ivanjevtic501@gmail.com>
2023-12-11 23:32:53 +0100

    Add dtos

--- src/main/java/rs/raf/demo/dto/PermissionDto.java
+++ src/main/java/rs/raf/demo/dto/PermissionDto.java
@@ -1,4 +1,13 @@
 package rs.raf.demo.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import rs.raf.demo.utils.PermissionType;
+
+@Getter
+@Setter
 public class PermissionDto {
+    PermissionType type;
+    String name;
+    String description;
 }
--- src/main/java/rs/raf/demo/dto/UserDto.java
+++ src/main/java/rs/raf/demo/dto/UserDto.java
@@ -1,4 +1,16 @@
 package rs.raf.demo.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
 public class UserDto {
+    private String username;
+    private String password;
+    private String email;
+    private Set<PermissionDto> permissions = new HashSet<>();
 }
