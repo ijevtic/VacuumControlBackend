b117dec6742a1b12eefaea14e43d98fc6655949d
Ivan Jevtic <ivanjevtic501@gmail.com>
2023-12-11 23:33:05 +0100

    Add mappers

--- src/main/java/rs/raf/demo/mapper/PermissionMapper.java
+++ src/main/java/rs/raf/demo/mapper/PermissionMapper.java
@@ -1,4 +1,44 @@
 package rs.raf.demo.mapper;
 
+import org.springframework.stereotype.Component;
+import rs.raf.demo.dto.PermissionDto;
+import rs.raf.demo.model.Permission;
+import rs.raf.demo.utils.PermissionType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
 public class PermissionMapper {
+    Map<Long, PermissionType> numberToType = Map.of(
+            0L, PermissionType.CREATE,
+            1L, PermissionType.READ,
+            2L, PermissionType.UPDATE,
+            3L, PermissionType.DELETE
+    );
+
+    Map<PermissionType, Long> typeToNumber = new HashMap<>();
+
+    {
+        for (Map.Entry<Long, PermissionType> entry : numberToType.entrySet()) {
+            typeToNumber.put(entry.getValue(), entry.getKey());
+        }
+    }
+
+    public PermissionDto toDto(Permission permission) {
+        PermissionDto dto = new PermissionDto();
+        dto.setName(permission.getName());
+        dto.setDescription(permission.getDescription());
+        dto.setType(numberToType.get(permission.getPermissionId()));
+        return dto;
+    }
+
+    public Permission toEntity(PermissionDto dto) {
+        Permission permission = new Permission();
+        permission.setName(dto.getName());
+        permission.setDescription(dto.getDescription());
+        permission.setPermissionId(typeToNumber.get(dto.getType()));
+        return permission;
+    }
+
 }
--- src/main/java/rs/raf/demo/mapper/UserMapper.java
+++ src/main/java/rs/raf/demo/mapper/UserMapper.java
@@ -1,4 +1,34 @@
 package rs.raf.demo.mapper;
 
+import org.springframework.stereotype.Component;
+import rs.raf.demo.dto.UserDto;
+import rs.raf.demo.model.User;
+
+import java.util.stream.Collectors;
+
+@Component
 public class UserMapper {
+    private PermissionMapper permissionMapper;
+
+    public UserMapper(PermissionMapper permissionMapper) {
+        this.permissionMapper = permissionMapper;
+    }
+
+    public UserDto toDto(User user) {
+        UserDto dto = new UserDto();
+        dto.setUsername(user.getUsername());
+        dto.setPassword(user.getPassword());
+        dto.setEmail(user.getEmail());
+        dto.setPermissions(user.getPermissions().stream().map(permissionMapper::toDto).collect(Collectors.toSet()));
+        return dto;
+    }
+
+    public User toEntity(UserDto dto) {
+        User user = new User();
+        user.setUsername(dto.getUsername());
+        user.setPassword(dto.getPassword());
+        user.setEmail(dto.getEmail());
+        user.setPermissions(dto.getPermissions().stream().map(permissionMapper::toEntity).collect(Collectors.toSet()));
+        return user;
+    }
 }
