6770f92e43d0b5c566ad6b71f0e588c761ec1b14
Ivan Jevtic <ivanjevtic501@gmail.com>
2023-12-13 11:38:23 +0100

    Add CredentialsDto

--- src/main/java/rs/raf/demo/dto/UserCredentialsDto.java
+++ src/main/java/rs/raf/demo/dto/UserCredentialsDto.java
@@ -1,4 +1,11 @@
 package rs.raf.demo.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserCredentialsDto {
+    private String email;
+    private String password;
 }
